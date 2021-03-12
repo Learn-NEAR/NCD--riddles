d8bc035c19dbb3595b5764a0d6d3376693cf8e0d
Cheng JIANG <alex_cj96@foxmail.com>
2021-03-12 21:15:52 +0800

    fix compiling

--- contract/compile.js
+++ contract/compile.js
@@ -42,18 +42,11 @@ const { code } = sh.exec(buildCmd)
 if (code === 0 && calledFromDir !== __dirname) {
   const linkDir = `${calledFromDir}/out`
   const link = `${calledFromDir}/out/main.wasm`
-  const packageName = require('fs')
-    .readFileSync(`${__dirname}/Cargo.toml`)
-    .toString()
-    .match(/name = "([^"]+)"/)[1]
-
-  const outFile = `./target/wasm32-unknown-unknown/${
-    debug ? 'debug' : 'release'
-  }/${packageName}.wasm`
+  const packageName = require('fs').readFileSync(`${__dirname}/Cargo.toml`).toString().match(/name = "([^"]+)"/)[1]
+  const outFile = `./target/wasm32-unknown-unknown/${debug ? 'debug' : 'release'}/${packageName}.wasm`
   sh.mkdir('-p', linkDir)
   sh.rm('-f', link)
-  const linkPath = path.resolve(path.relative(linkDir, outFile))
-  console.log(linkPath + ' -> ' + link)
+  const linkPath = path.relative(linkDir, outFile)
 
   sh.ln('-s', linkPath, link)
 }
